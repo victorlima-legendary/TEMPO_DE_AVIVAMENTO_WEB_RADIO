66ca06ced81e5226fe434c89de6e968cf58a9c02
Vitor Lima <86374673+victorlima-legendary@users.noreply.github.com>
2025-04-21 13:21:20 -0300

    AUTH UPDATE

--- src/js/limao.js
+++ src/js/limao.js
@@ -1,2 +1,13 @@
-// src/js/token.js
-export const GITHUB_TOKEN = "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";
+// limao.js
+
+// Função para obter o token do GitHub
+export async function getGithubToken() {
+    try {
+        const res = await fetch("https://workers-playground-curly-moon-482e.vitorlimagodoy8.workers.dev");
+        const data = await res.json();
+        return data.token;  // Retorna o token vindo da URL externa
+    } catch (e) {
+        console.error("Erro ao buscar token:", e);
+        return null;  // Retorna null caso ocorra erro na busca
+    }
+}
