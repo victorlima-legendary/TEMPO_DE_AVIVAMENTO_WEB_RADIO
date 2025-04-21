d516405ce01671c175048b14aa6ee17aa6128282
Vitor Lima <86374673+victorlima-legendary@users.noreply.github.com>
2025-04-20 22:14:32 -0300

    ADDED FUNCTIONAL STATE FOR "PALAVRA" DIV

---
+++ src/js/palavra.js
@@ -0,0 +1,23 @@
+async function atualizarFrase() {
+    const qTag = document.getElementById("frase");
+
+    try {
+        const res = await fetch("https://api.github.com/repos/victorlima-legendary/JSON_RADIO/contents/palavra.json");
+        const data = await res.json();
+
+        const base64 = data.content.replace(/\n/g, "");
+        const bytes = Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+        const conteudo = new TextDecoder("utf-8").decode(bytes);
+        const json = JSON.parse(conteudo);
+
+
+        qTag.textContent = json.frase || "Frase não encontrada.";
+    } catch (e) {
+        console.error("Erro ao buscar frase:", e);
+        qTag.textContent = "Erro ao carregar.";
+    }
+}
+
+
+atualizarFrase();
+setInterval(atualizarFrase, 1000);
