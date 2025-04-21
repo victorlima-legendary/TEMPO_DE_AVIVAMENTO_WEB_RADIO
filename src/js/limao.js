c14feffdefbbac41e2a2d601e122ccf72d2352e4
Vitor Lima <86374673+victorlima-legendary@users.noreply.github.com>
2025-04-21 13:25:33 -0300

    Update limao.js

--- src/js/limao.js
+++ src/js/limao.js
@@ -1,13 +1,12 @@
 // limao.js
 
-// Função para obter o token do GitHub
 export async function getGithubToken() {
     try {
         const res = await fetch("https://workers-playground-curly-moon-482e.vitorlimagodoy8.workers.dev");
         const data = await res.json();
-        return data.token;  // Retorna o token vindo da URL externa
+        return data.token;
     } catch (e) {
         console.error("Erro ao buscar token:", e);
-        return null;  // Retorna null caso ocorra erro na busca
+        return null;
     }
 }
